ca26873fc0ffc8b027258163662c6b4913ff3c1e
박수현 <dogemad93@gmail.com>
2020-01-14 14:54:16 +0900

    ADD: scripts

---
+++ inspect.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+	apps: [
+		{
+			name: 'express-dogemad',
+			script: 'bin/www',
+			// Inspect for chrome debugger
+			node_args: ['--inspect'],
+			log_date_format: 'YYYY-MM-DD HH:mm:ss',
+			env: {
+				NODE_ENV: 'dev'
+			},
+			env_production: {
+				NODE_ENV: 'production'
+			},
+			env_stage: {
+				NODE_ENV: 'stage'
+			},
+			env_ote: {
+				NODE_ENV: 'ote'
+			},
+			env_local: {
+				NODE_ENV: 'local'
+			}
+		}
+	]
+};
